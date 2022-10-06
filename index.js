13e55675c3fb1ea0a27962de3a9842bf83b7acbe
Christopher Hillman <katie5five.5@gmail.com>
2022-10-06 17:27:14 +0200

    Design finished w/o responsive

---
+++ index.js
@@ -0,0 +1,9 @@
+// GLOBAL VARIABLES
+
+let carouselCurrent;
+
+// CAROUSEL
+
+let carouselImage = "https://cdn.vox-cdn.com/thumbor/fS_fixiYyJ5ZmFzCKBlxuWmf4Rg=/0x248:3502x2082/fit-in/1200x630/cdn0.vox-cdn.com/uploads/chorus_asset/file/8108313/irish_bar_lead_photo.jpg";
+
+// document.getElementById("carousel__image").innerHTML = `<img src="${carouselImage}" alt="" class="carousel__image--pic">`
