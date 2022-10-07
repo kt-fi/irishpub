b2e0efa6e80a5d4c4ab354013f2a64b4aa6506e8
Christopher Hillman <katie5five.5@gmail.com>
2022-10-07 16:03:06 +0200

    added function to Carousel

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 // GLOBAL VARIABLES
 
-let carouselCurrent;
+
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const carLeft = document.getElementById("carLeft");
+const carRight = document.getElementById("carRight");
 
 // CAROUSEL
 
-let carouselImage = "https://cdn.vox-cdn.com/thumbor/fS_fixiYyJ5ZmFzCKBlxuWmf4Rg=/0x248:3502x2082/fit-in/1200x630/cdn0.vox-cdn.com/uploads/chorus_asset/file/8108313/irish_bar_lead_photo.jpg";
 
-// document.getElementById("carousel__image").innerHTML = `<img src="${carouselImage}" alt="" class="carousel__image--pic">`
+carLeft.addEventListener('click', (event)=>{
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+})
+
+carRight.addEventListener('click', (event)=>{
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+})
+
